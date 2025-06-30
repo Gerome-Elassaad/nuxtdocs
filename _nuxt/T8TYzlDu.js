7ceddf80edc21d2649beb66203f335227ea5ca0e
Gerome-Elassaad <grambo328@gmail.com>
2025-06-30 14:25:51 +0000

    Deploying to gh-pages from @ Gerome-Elassaad/docs@cec5767698eb716a582343c111782df2dcd47b6a 🚀

--- _nuxt/T8TYzlDu.js
+++
@@ -1,1 +0,0 @@
-import{r as s}from"./BqXDAXSb.js";import t from"./Dwwil6xh.js";import{z as e,H as a,i as p,w as c,W as i}from"./lU38qAMc.js";import"./C2bHYaEW.js";const d={__name:"ProseTip",setup(n){const o=e();return(r,f)=>(p(),a(t,{color:"success",icon:i(o).ui.icons.tip},{default:c(()=>[s(r.$slots,"default",{mdcUnwrap:"p"})]),_:3},8,["icon"]))}};export{d as default};
